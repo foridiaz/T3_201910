a7f9e624732e50503381b6f6db061b1f50f52a8d
unknown <VAU0612@ad.uniandes.edu.co>
2019-02-08 13:35:28 -0500

    Avance nodo 1 XD

---
+++ src/model/data_structures/Nodo.java
@@ -0,0 +1,17 @@
+package model.data_structures;
+
+public class Nodo<T> 
+{
+	//Atributos
+	/**
+	 * 
+	 */
+	private T elemento;
+	
+	private boolean primero;
+	
+	private boolean ultimo;
+	
+	private Nodo<T> siguiente;
+
+}
