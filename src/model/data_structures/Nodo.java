ad007970e27af044412d6bcabf1a30dc98e6db8d
Juan Sebastian Gonzalez Rojas <js.gonzalez15@sis.virtual.uniandes.edu.co>
2019-02-11 16:40:56 -0500

    doblemente encadenado listo

--- src/model/data_structures/Nodo.java
+++ src/model/data_structures/Nodo.java
@@ -1,7 +1,7 @@
 package model.data_structures;
 
 /** Clase que permite el almacenamiento de objetos genéricos en colas y filas
- *  Implementa los métodos necesarios para ambas estructuras de datos
+ *  Implementa los métodos necesarios para ambas estructuras de datos ()
  */
 public class Nodo<T> 
 {
@@ -11,30 +11,36 @@ public class Nodo<T>
 	 * objeto genérico almacenado por el nodo. 
 	 */
 	private T elemento;
-	
+
 	/**
 	 * booleano que determina si el nodo ocupa la posición especial primero. 
 	 */
 	private boolean primero;
-	
+
 	/**
 	 * booleano que determina si el nodo ocupa la posición especial último.
 	 */
 	private boolean ultimo;
-	
+
 	/**
-	 * Relación de cola/pila en lista sencillamente encadenada
+	 * Relación de cola/pila en lista doblemente encadenada
 	 */
 	private Nodo<T> siguiente;
+	
+	/**
+	 * Relación de cola/pila en lista doblemente encadenada
+	 */
+	private Nodo<T> anterior;
 
 	//Método Constructor
-	public Nodo<T> (T pElemento, boolean pPrimero, boolean pUltimo)
+	public Nodo (T pElemento, boolean pPrimero, boolean pUltimo)
 	{
-	 //Para construir un nodo es necesario conocer el elemento de entrada, si es primero o ultimo.
-	 elemento= pElemento;
-	 primero = pPrimero;
-	 ultimo  = pUltimo;
-	 siguiente = null; //El nodo siguiente no es contruido pero debe ser inmediatamente asignado
+		//Para construir un nodo es necesario conocer el elemento de entrada, si es primero o ultimo.
+		elemento= pElemento;
+		primero = pPrimero;
+		ultimo  = pUltimo;
+		anterior = null; //El nodo siguiente no es contruido pero debe ser inmediatamente asignado
+		siguiente = null; //El nodo siguiente no es contruido pero debe ser inmediatamente asignado
 	}
 
 	//Métodos del nodo
@@ -44,7 +50,15 @@ public class Nodo<T>
 	 */
 	public void setSiguiente(Nodo<T> nuevoSiguiente)
 	{
-	 siguiente =nuevoSiguiente;
+		siguiente =nuevoSiguiente;
+	}
+	
+	/**
+	 * método encargado de reasignar el nodo siguiente. 
+	 */
+	public void setAnterior(Nodo<T> nuevoAnterior)
+	{
+		siguiente =nuevoAnterior;
 	}
 
 	/**
@@ -52,15 +66,23 @@ public class Nodo<T>
 	 */
 	public boolean esPrimero()
 	{
-	 return primero;
+		return primero;
 	}
 
 	/**
 	 * método que retorna el siguiente nodo. 
 	 */
-	public boolean darSiguiente()
+	public Nodo<T> darSiguiente()
 	{
-	 return siguiente;
+		return siguiente;
+	}
+	
+	/**
+	 * método que retorna el siguiente nodo. 
+	 */
+	public Nodo<T> darAnterior()
+	{
+		return anterior;
 	}
 
 	/**
@@ -68,7 +90,15 @@ public class Nodo<T>
 	 */
 	public void setPrimero(boolean pPrimero)
 	{
-	  primero = pPrimero;
+		primero = pPrimero;
+	}
+	
+	/**
+	 * método que retorna el siguiente nodo. 
+	 */
+	public void setUltimo(boolean pUltimo)
+	{
+		ultimo = pUltimo;
 	}
 
 	/**
@@ -77,16 +107,18 @@ public class Nodo<T>
 	 */
 	public void enqueue(Nodo<T> nuevoUltimo)
 	{
-	 if(ultimo)
-	 {
-	  nuevoUltimo.setSiguiente(siguiente); //el siguiente nodo al último es el primero, por lo tanto se añade inicialmente
-	  setSiguiente(nuevoUltimo);
-	  ultimo = false; //se actualiza el estado del nodo
-	 }
-	 else
-	 {
-	  siguiente.enqueue(nuevoUltimo);
-	 }
+		if(ultimo)
+		{
+			nuevoUltimo.setSiguiente(siguiente); //el siguiente nodo al último es el primero, por lo tanto se añade inicialmente
+			nuevoUltimo.setAnterior(this); //el nodo actual es el nuevo anterior al nuevo nodo
+			siguiente.setAnterior(nuevoUltimo); //el nodo primero tiene como nuevo anterior el nuevo nodo
+			setSiguiente(nuevoUltimo);
+			ultimo = false; //se actualiza el estado del nodo
+		}
+		else
+		{
+			siguiente.enqueue(nuevoUltimo);
+		}
 	}
 
 	/**
@@ -94,21 +126,20 @@ public class Nodo<T>
 	 */
 	public void dequeue()
 	{
-		// ESTE METODO TIENE ORDEN N DE OPERACION, SERIA MAS RAPIDO SI FUERA DOBLEMENTE ENCADENADO PERO PUES HABRIA QUE CAMBIARLO TODO JAJAJ
-	 if(siguiente.esPrimero())
-	 {
-	  siguiente.darSiguiente().setPrimero(true);//se actualiza el estado del segundo nodo a primero
-	  setSiguiente(siguiente.darSiguiente()); //se elimina la relación del último con el antiguo primero.
-	 }
-	 else
-	 {
-	  siguiente.dequeue();
-	 }
+		
+		if(siguiente.esPrimero())
+		{
+			siguiente.darSiguiente().setPrimero(true);//se actualiza el estado del segundo nodo a primero
+			siguiente.setAnterior(anterior); //el anterior es el nuevo anterior del segundo nodo  
+			anterior.setSiguiente(siguiente); //se elimina la relación del último con el antiguo primero.
+		}
+		else
+		{
+			siguiente.dequeue();
+		}
 	}
 
-	/**
-	 * objeto genérico almacenado por el nodo. 
-	 */
-	
+
+
 
 }
