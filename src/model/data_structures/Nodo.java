c9132207fc73d12f960f4eef335037104a21b39f
jsgonzalez15b <47197962+jsgonzalez15b@users.noreply.github.com>
2019-02-12 18:30:21 -0500

    Update Nodo.java

--- src/model/data_structures/Nodo.java
+++ src/model/data_structures/Nodo.java
@@ -1,52 +1,34 @@
 package model.data_structures;
 
-/** Clase que permite el almacenamiento de objetos genéricos en colas y filas
- *  Implementa los métodos necesarios para ambas estructuras de datos ()
+/** Clase que permite el almacenamiento de objetos genÃ©ricos en colas y filas
+ *  Implementa los mÃ©todos necesarios para ambas estructuras de datos ()
  */
 public class Nodo<T> 
 {
 	//Atributos
 
 	/**
-	 * objeto genérico almacenado por el nodo. 
+	 * objeto genÃ©rico almacenado por el nodo. 
 	 */
 	private T elemento;
 
 	/**
-	 * booleano que determina si el nodo ocupa la posición especial primero. 
-	 */
-	private boolean primero;
-
-	/**
-	 * booleano que determina si el nodo ocupa la posición especial último.
-	 */
-	private boolean ultimo;
-
-	/**
-	 * Relación de cola/pila en lista doblemente encadenada
+	 * RelaciÃ³n de cola/pila en lista simplemente encadenada
 	 */
 	private Nodo<T> siguiente;
 	
-	/**
-	 * Relación de cola/pila en lista doblemente encadenada
-	 */
-	private Nodo<T> anterior;
-
-	//Método Constructor
-	public Nodo (T pElemento, boolean pPrimero, boolean pUltimo)
+	//MÃ©todo Constructor
+	public Nodo (T pElemento)
 	{
 		//Para construir un nodo es necesario conocer el elemento de entrada, si es primero o ultimo.
 		elemento= pElemento;
-		primero = pPrimero;
-		ultimo  = pUltimo;
-		anterior = null; //El nodo siguiente no es contruido pero debe ser inmediatamente asignado
 		siguiente = null; //El nodo siguiente no es contruido pero debe ser inmediatamente asignado
 	}
 
-	//Métodos del nodo
+	//MÃ©todos del nodo
 
 	/**
-	 * método encargado de reasignar el nodo siguiente. 
+	 * mÃ©todo encargado de reasignar el nodo siguiente. 
 	 */
 	public void setSiguiente(Nodo<T> nuevoSiguiente)
 	{
@@ -54,23 +36,15 @@ public class Nodo<T>
 	}
 	
 	/**
-	 * método encargado de reasignar el nodo siguiente. 
+	 * mÃ©todo encargado de retornar el elemento almacenado en el nodo. 
 	 */
-	public void setAnterior(Nodo<T> nuevoAnterior)
+	public T darElemento()
 	{
-		siguiente =nuevoAnterior;
+		return elemento;
 	}
 
 	/**
-	 * método que determina si este nodo es el primero. 
-	 */
-	public boolean esPrimero()
-	{
-		return primero;
-	}
-
-	/**
-	 * método que retorna el siguiente nodo. 
+	 * mÃ©todo que retorna el siguiente nodo. 
 	 */
 	public Nodo<T> darSiguiente()
 	{
@@ -78,65 +52,25 @@ public class Nodo<T>
 	}
 	
 	/**
-	 * método que retorna el siguiente nodo. 
-	 */
-	public Nodo<T> darAnterior()
-	{
-		return anterior;
-	}
-
-	/**
-	 * método que retorna el siguiente nodo. 
-	 */
-	public void setPrimero(boolean pPrimero)
-	{
-		primero = pPrimero;
-	}
-	
-	/**
-	 * método que retorna el siguiente nodo. 
-	 */
-	public void setUltimo(boolean pUltimo)
-	{
-		ultimo = pUltimo;
-	}
-
-	/**
-	 * método recursivo para enqueue un nuevo elemento. 
+	 * mÃ©todo para enqueue un nuevo elemento estando en el Ãºltimo nodo. 
 	 *@param  nuevoUltimo tiene inicializacion primero= false, ultimo =true
 	 */
-	public void enqueue(Nodo<T> nuevoUltimo)
+	public void enqueue(T elemento)
 	{
-		if(ultimo)
-		{
-			nuevoUltimo.setSiguiente(siguiente); //el siguiente nodo al último es el primero, por lo tanto se añade inicialmente
-			nuevoUltimo.setAnterior(this); //el nodo actual es el nuevo anterior al nuevo nodo
-			siguiente.setAnterior(nuevoUltimo); //el nodo primero tiene como nuevo anterior el nuevo nodo
-			setSiguiente(nuevoUltimo);
-			ultimo = false; //se actualiza el estado del nodo
-		}
-		else
-		{
-			siguiente.enqueue(nuevoUltimo);
-		}
+		Nodo<T> nuevoUltimo= new Nodo(elemento);
+		nuevoUltimo.setSiguiente(siguiente); //el siguiente nodo al Ãºltimo es el primero, por lo tanto se aÃ±ade inicialmente
+		setSiguiente(nuevoUltimo); //el nodo primero tiene como nuevo anterior el nuevo nodo		
 	}
 
 	/**
-	 * método recursivo para dequeue un nuevo elemento. 
+	 * mÃ©todo para dequeue el primer elemento estando en el Ãºltimo elemento. 
+	 *@return elemento retorna el elemento T del nodo eliminado.
 	 */
-	public void dequeue()
+	public T dequeue()
 	{
-		
-		if(siguiente.esPrimero())
-		{
-			siguiente.darSiguiente().setPrimero(true);//se actualiza el estado del segundo nodo a primero
-			siguiente.setAnterior(anterior); //el anterior es el nuevo anterior del segundo nodo  
-			anterior.setSiguiente(siguiente); //se elimina la relación del último con el antiguo primero.
-		}
-		else
-		{
-			siguiente.dequeue();
-		}
+		T elElemento = siguiente.darElemento();
+		setSiguiente(siguiente.darSiguiente()); //se elimina la relaciÃ³n del Ãºltimo con el antiguo primero.
+		return elElemento;
 	}
 
 
